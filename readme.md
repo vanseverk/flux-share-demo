d791eb58fcda5811036f181c3e1ba89bb44ebf6f
vanseverk <vanseverk@gmail.com>
2018-11-02 11:40:47 +0100

    Demo for article https://www.reactiveprogramming.be/flux-sharing-from-one-to-many/

---
+++ readme.md
@@ -0,0 +1,2 @@
+Demo application for article: 
+[Flux Sharing: From one to many](https://www.reactiveprogramming.be/flux-sharing-from-one-to-many/)
---
+++ src/main/java/be/reactiveprogrammming/demo/fluxsharing/FluxShareTestApplication.java
@@ -0,0 +1,26 @@
+package be.reactiveprogrammming.demo.fluxsharing;
+
+import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import reactor.core.publisher.Flux;
+
+public class FluxShareTestApplication {
+
+  public static void main(String[] args) throws InterruptedException {
+    fluxShareMultipleSubscribers(50000);
+  }
+
+  private static void fluxShareMultipleSubscribers(int numberOfSubscriptions) throws InterruptedException {
+    Flux<Long> startFlux = Flux.interval(Duration.ofMillis(1000));
+
+    for (int i = 0; i < numberOfSubscriptions; i++) {
+      final int subscriptionNumber = i;
+      Flux outputFlux = startFlux.share();
+      outputFlux.subscribe(out -> System.out.println("Flux " + subscriptionNumber + " " + out));
+    }
+
+    new CountDownLatch(1).await(100, TimeUnit.SECONDS);
+  }
+
+}
